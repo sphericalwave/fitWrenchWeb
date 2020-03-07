e183b3b3de9f6136afa0e3c1d74dc37421ee2344
aaronium112 <aaron@sphericalwave.com>
2020-03-07 18:21:15 -0400

    changed project name to FitWrenchWeb

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "FitWrench",
+  name: "FitWrenchWeb",
   dependencies: [
     .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
